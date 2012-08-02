5005212ad37d6be1f46eefaeac66cb399cfcce1c
Abu Salam Parvez Alam <abu@paschimmedinipur.org>
2012-08-02 17:03:50 +0530

    Added pdf.js submodule
    Only pdf file will be shown. and addition of pdf.js script, made the
    list ordered.

--- index.php
+++ index.php
@@ -1,10 +1,30 @@
 <!DOCTYPE HTML>
 <html>
-<head></head>
+<head>
+<title>PDF Page Counter</title>
+<style>
+.example {
+	padding: 10px;
+	border: 1px solid #ccc
+}
+
+#drop_zone {
+	border: 2px dashed #bbb;
+	-moz-border-radius: 5px;
+	-webkit-border-radius: 5px;
+	border-radius: 5px;
+	padding: 25px;
+	text-align: center;
+	font: 20pt bold, "Vollkorn";
+	color: #bbb
+}
+</style>
+<script src="pdf-js/build/pdf.js"></script>
+</head>
 <body>
+<div class="example">
 <div id="drop_zone">Drop files here</div>
 <output id="list"></output>
-
 <script>
   function handleFileSelect(evt) {
     evt.stopPropagation();
@@ -15,12 +35,12 @@
     // files is a FileList of File objects. List some properties.
     var output = [];
     for (var i = 0, f; f = files[i]; i++) {
-      output.push('<li><strong>', escape(f.name), '</strong> (', f.type || 'n/a', ') - ',
-                  f.size, ' bytes, last modified: ',
-                  f.lastModifiedDate ? f.lastModifiedDate.toLocaleDateString() : 'n/a',
-                  '</li>');
+      if(f.type.match('application/pdf'))
+      {
+          output.push('<li><strong>', escape(f.name), '</strong></li>');
+      }
     }
-    document.getElementById('list').innerHTML = '<ul>' + output.join('') + '</ul>';
+    document.getElementById('list').innerHTML = '<ol>' + output.join('') + '</ol>';
   }
 
   function handleDragOver(evt) {
@@ -34,5 +54,6 @@
   dropZone.addEventListener('dragover', handleDragOver, false);
   dropZone.addEventListener('drop', handleFileSelect, false);
 </script>
+</div>
 </body>
 </html>
